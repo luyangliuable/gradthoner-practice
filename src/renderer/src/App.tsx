a8f8bae6f21a3bbf2a50604c8e887749c0055378
Liu <Lucas.Liu@cba.com.au>
2024-08-05 15:41:13 +1000

    Added `tailwind` and removed boilerplate code.

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,35 +1,27 @@
-import Versions from "./components/Versions";
-import electronLogo from "./assets/electron.svg";
 import * as A from "@shared/types";
+import { Button, Flex } from "antd";
 
 function App(): JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send("ping");
 
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
+    <main className="h-[100vh] w-[100vw] position-relative flex flex-col justify-evenly items-center">
+      <h1 className="text-5xl text-[var(--ev-c-text-6)] font-extrabold">Batch Git Repo Tool</h1>
+      <Flex gap="middle" className="w-full" justify="center" wrap>
+        <Button className="bg-[var(--color-button)] rounded-2xl p-5" type="primary">Github Access Token</Button>
+        <Button className="bg-[var(--color-button)] rounded-2xl p-5" type="primary">List Repositories</Button>
+        <Button className="bg-[var(--color-button)] rounded-2xl p-5" type="primary">Add Files</Button>
+        <Button className="bg-[var(--color-button)] rounded-2xl p-5" type="primary">Add Team</Button>
+      </Flex>
+      <Flex className="w-[50%] text-center" gap="middle" justify="center" align="center" vertical>
+        <span>
+          This tool aims to provide a graphical interface with ghe-tools.go and the Github REST API to process multiple repositories in batch which can be a time-consuming task.
+        </span>
+        <span>
+          For contribution and adding features to this app - repository
+        </span>
+      </Flex>
+    </main>
   );
 }
 
